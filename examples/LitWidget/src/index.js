b3417cad2760a06990c36a629d301911e1ef71ba
Viktor Lukashov <viktor.lukashov@happeo.com>
2021-09-04 22:28:42 +0300

    add a Lit example with JS

--- examples/LitWidget/src/index.js
+++ examples/LitWidget/src/index.js
@@ -1,63 +1,65 @@
+import { LitElement, html, css } from "lit";
+import widgetSDK from "@happeo/widget-sdk";
+
 // Create a class for the element
-class happeoCustomWidget extends HTMLElement {
-  constructor() {
-    // Always call super first in constructor
-    super();
-
-    // Create a shadow root
-    const shadow = this.attachShadow({ mode: "open" });
-
-    // Create spans
-    const wrapper = document.createElement("div");
-    wrapper.setAttribute("class", "wrapper");
-
-    const title = document.createElement("h1");
-    title.innerText = "Happeo custom widget";
-
-    const text = document.createElement("p");
-    text.innerText = "Useful resources";
-
-    const list = document.createElement("ul");
-    const item1 = document.createElement("li");
-    const item2 = document.createElement("li");
-    const item3 = document.createElement("li");
-
-    item1.innerHTML =
-      '<p><a href="https://github.com/happeo/custom-widget-templates" target="_blank">Custom widget templates</a></p>';
-    item2.innerHTML =
-      '<p><a href="https://github.com/happeo/widgets-sdk" target="_blank">Widget SDK</a></p>';
-    item1.innerHTML =
-      '<p><a href="https://uikit.happeo.com/" target="_blank">Happeo UI kit</a></p>';
-
-    list.appendChild(item1);
-    list.appendChild(item2);
-    list.appendChild(item3);
-
-    // Create some CSS to apply to the shadow dom
-    const style = document.createElement("style");
-
-    style.textContent = `
-          .wrapper {
-            position: relative;
-          }
-          p {
-            font-size: 16px;
-          }
-          h1, p + p {
-            margin-top: 4px;
-          }
-        `;
-
-    // Attach the created elements to the shadow dom
-    shadow.appendChild(wrapper);
-    wrapper.appendChild(title);
-    wrapper.appendChild(text);
-    wrapper.appendChild(list);
+class happeoCustomWidget extends LitElement {
+  static get properties() {
+    return {
+      widgetId: { type: String },
+      uniqueId: { type: String },
+      editMode: { type: String },
+      user: { state: true }
+    };
   }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.doInit();
+  }
+
+  async doInit() {
+    // Init API
+    const widgetApi = await widgetSDK.api.init(this.widgetId);
+
+    // Do stuff
+    this.user = await widgetApi.getCurrentUser();
+    console.log("I am all good and ready to go!", this.user);
+  };
+
+  render() {
+    return html`
+      <div class="wrapper">
+        <h1>Happeo custom widget</h1>
+        <p>
+          ${this.user && html`Hi, ${this.user.name.fullName}!` || html`initializing...`}
+        </p>
+        <p>
+          Useful resources
+        </p>
+        <ul>
+          <li><p><a href="https://github.com/happeo/custom-widget-templates" target="_blank">Custom widget templates</a></p></li>
+          <li><p><a href="https://github.com/happeo/widgets-sdk" target="_blank">Widget SDK</a></p></li>
+          <li><p><a href="https://uikit.happeo.com/" target="_blank">Happeo UI kit</a></p></li>
+        </ul>
+      </div>
+    `;
+  }
+
+  static styles = css`
+    .wrapper {
+      position: relative;
+    }
+    p {
+      font-size: 16px;
+    }
+    h1, p + p {
+      margin-top: 4px;
+    }
+  `;
 }
 
 // Define the new element
-const slug = "my-widget-slug";
+const slug = "vllitexample-6mb4xbbjfbfpkhzsatf5";
 
 window.customElements.get(slug) ||
   window.customElements.define(slug, happeoCustomWidget);

b7f9b0fa352368439ea83c3ef44897074bd43166
Viktor Lukashov <viktor.lukashov@happeo.com>
2021-09-04 23:28:53 +0300

    add Lit custom widget example
    Lit is a small (about 5KB) helper library for creating standard Web components (https://lit.dev/).
    Since Happeo Custom Widgets are implemented as web components, using Lit is a great option here.
    
    This commit add both a JavaScript and a TypeScript version of a custom widget built with Lit 2.

---
+++ Lit-TypeScript/src/index.ts
@@ -0,0 +1,90 @@
+import { LitElement, html, css } from "lit";
+import { property, state } from "lit/decorators";
+import widgetSDK from "@happeo/widget-sdk";
+import type { User } from "@happeo/widget-sdk/dist/interfaces";
+
+// Create a class for the element
+class happeoCustomWidget extends LitElement {
+  @property({ type: String })
+  widgetId = "";
+
+  @property({ type: String })
+  uniqueId = "";
+
+  @property({ type: Boolean })
+  editMode = false;
+
+  @state()
+  private user?: User;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.doInit();
+  }
+
+  async doInit() {
+    // Init API
+    const widgetApi = await widgetSDK.api.init(this.widgetId);
+
+    // Do stuff
+    this.user = await widgetApi.getCurrentUser();
+    console.log("I am all good and ready to go!", this.user);
+  }
+
+  render() {
+    return html`
+      <div class="wrapper">
+        <h1>Happeo custom widget</h1>
+        <p>
+          ${(this.user && html`Hi, ${this.user.name.fullName}!`) ||
+          html`initializing...`}
+        </p>
+        <p>Useful resources</p>
+        <ul>
+          <li>
+            <p>
+              <a
+                href="https://github.com/happeo/custom-widget-templates"
+                target="_blank"
+                >Custom widget templates</a
+              >
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="https://github.com/happeo/widgets-sdk" target="_blank"
+                >Widget SDK</a
+              >
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="https://uikit.happeo.com/" target="_blank"
+                >Happeo UI kit</a
+              >
+            </p>
+          </li>
+        </ul>
+      </div>
+    `;
+  }
+
+  static styles = css`
+    .wrapper {
+      position: relative;
+    }
+    p {
+      font-size: 16px;
+    }
+    h1,
+    p + p {
+      margin-top: 4px;
+    }
+  `;
+}
+
+// Define the new element
+const slug = "my-widget-slug";
+
+window.customElements.get(slug) ||
+  window.customElements.define(slug, happeoCustomWidget);
--- README.md
+++ README.md
@@ -33,6 +33,15 @@ Running `npm install && npm start`. That will start the dev server and serve the
 
 Note that this will only serve the JS -file and no html is being served. So next we need to run this inside Happeo.
 
+#### Lit
+
+[Lit](https://lit.dev/) is a small (about 5KB) helper library for creating standard Web components.
+This option is quite close to vanilla JS, but is much easier to read and write since it uses a React-like syntax for component templates.
+
+Start by opening either a Lit-JavaScript or a Lit-TypeScript folder, opening the `src/index.js`-file and replace `"my-widget-slug"` with the `slug` you got from step 2.
+
+Running `npm install && npm start`. That will start the dev server and serve the bundle.js -file from localhost:8080/bundle.js.
+
 ### Running inside Happeo
 
 Now that you have your custom widget created and it is in **DEVELOPMENT** -mode, you can add it to Pages. Note that the widget is only visible to administrators when in **DEVELOPMENT** and **TESTING** -modes.

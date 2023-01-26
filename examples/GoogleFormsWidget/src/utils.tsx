c71cc4b41537419ec1ae2cf390bc0a5f77b42e43
Gaurav Bhorkar <gaurav.bhorkar@happeo.com>
2023-01-26 16:38:56 +0200

    chore: add readme and clean up examples folder

--- examples/GoogleFormsWidget/src/utils.tsx
+++
@@ -1,8 +0,0 @@
-import { decode } from "html-entities";
-
-export const decodeHtmlEntities = (data: string) => decode(data);
-
-export const getGoogleId = (text: string) => {
-  const results = text.match(/[-\w]{25,}/);
-  return results && results[0];
-};
--- examples/Mermaid.js/public/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Widget from "../src/Widget";
-
-ReactDOM.render(
-  <Widget id={"123"} editMode />,
-  document.getElementById("root"),
-);
